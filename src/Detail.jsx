054604efd41597f37bee820779e62a40db5cd00b
Ratthicha Parinthip <92798644+premepreme@users.noreply.github.com>
2023-02-10 14:34:36 +0700

    add water button

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,13 +1,17 @@
 import Card from "./components/card";
-import pic from "./components/default.png";
 import Radio from "./components/radio-button";
+import pic from "./components/water-b.png";
+import "./App.css"
 
 function Detail() {
   return (
     <div >
       < Radio/>
+      <button type="button" class="btn btn-danger " >
+        <img src={pic} className='water'/>
+      </button>
       <h1>Tree name</h1>
-      <Card img={'https://cdn-icons-png.flaticon.com/512/1752/1752941.png'}/>
+      <Card img={'https://cdn-icons-png.flaticon.com/512/1752/1752941.png'} />
       <div className="detail" >
         <h4>Temperature: </h4>
         </div>
