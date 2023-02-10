08fb8568fece9e42471744cc7671d85cf00b0ff3
Hirasawa Ishigawa <hirasawa_au@yahoo.co.jp>
2023-02-10 17:40:03 +0700

    feat: Post to backend

--- src/components/card_home.jsx
+++ src/components/card_home.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import '../styles/card_home.css'
-import pic from '../components/pic02.png'
+import React from "react";
+import "../styles/card_home.css";
+import pic from "../components/pic02.png";
 
-const Card1 = ({ id, date, moist, temp, light}) => {
+const Card1 = ({ id, date, moist, temp, light }) => {
   return (
-    <div className='card1'>
+    <div className="card1">
       <h1>ID: {id}</h1>
-      <img src={pic} alt="plant" />
-      <div className='det'>Date: {date}</div>
-      <div className='det'>Moisture: {moist}</div>
-      <div className='det'>Temperature: {temp}</div>
-      <div className='det'>Light: {light}</div>
-      </div>
-  )
-}
+      <img
+        src={pic}
+        alt="plant"
+        style={{
+          marginTop: "10px",
+          marginBottom: "10px",
+        }}
+      />
+      <div className="det">Date: {new Date(date).toLocaleDateString()}</div>
+      <div className="det">Time: {new Date(date).toLocaleTimeString()}</div>
+      <div className="det">Moisture: {moist}</div>
+      <div className="det">Temperature: {temp}</div>
+      <div className="det">Light: {light}</div>
+    </div>
+  );
+};
 
-export default Card1
+export default Card1;
