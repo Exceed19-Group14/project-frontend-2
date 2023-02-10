640c93532877c93c57637193dee4befff8b5c197
Hirasawa Ishigawa <hirasawa_au@yahoo.co.jp>
2023-02-10 14:07:03 +0700

    feat: Migrate to vite (#1)
    * feat: Migrate to vite
    
    * Remove react-script

---
+++ src/components/TextBox.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/TextBox.css";
+
+function TextBox({ inside }) {
+  return <input className="TextBox" type="text" placeholder={inside}></input>;
+}
+
+export default TextBox;
